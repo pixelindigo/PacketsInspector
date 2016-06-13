2b24fac447761f0d979345abac3fbb4af35d0a7f
PixelIndigo <m4s4n0bu@gmail.com>
2016-06-13 19:40:04 +0300

    Added ActionBar Up navigation button

--- app/src/main/java/com/pixvoxsoftware/packetsinspector/MainActivity.java
+++ app/src/main/java/com/pixvoxsoftware/packetsinspector/MainActivity.java
@@ -2,6 +2,7 @@ package com.pixvoxsoftware.packetsinspector;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -12,7 +13,8 @@ import java.io.File;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements PacketsListFragment.OnShowPacketListener, PacketDetailsFragment.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements PacketsListFragment.OnShowPacketListener, PacketDetailsFragment.OnFragmentInteractionListener,
+        FragmentManager.OnBackStackChangedListener{
 
     public final static String SELECTED_FILE = "selected_file";
 
@@ -47,6 +49,10 @@ public class MainActivity extends AppCompatActivity implements PacketsListFragme
                 getSupportActionBar().setTitle(file.getName());
             }
         }
+
+        getSupportFragmentManager().addOnBackStackChangedListener(this);
+
+        configureUpButton();
     }
 
     @Override
@@ -64,5 +70,23 @@ public class MainActivity extends AppCompatActivity implements PacketsListFragme
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    private void configureUpButton() {
+        if (getSupportActionBar() != null) {
+            boolean enabled = getSupportFragmentManager().getBackStackEntryCount() != 0;
+            getSupportActionBar().setDisplayHomeAsUpEnabled(enabled);
+        }
+    }
+
+    @Override
+    public void onBackStackChanged() {
+        configureUpButton();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        getSupportFragmentManager().popBackStack();
+        return true;
     }
 }
